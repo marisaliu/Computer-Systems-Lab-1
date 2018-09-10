49582cef302b1b95ef73d140d986098a258267a0
Dylan Banh <dylanb271@gmail.com>
2018-09-10 21:26:26 +0100

    added logical log function

--- CacheSimulatorDMS.c
+++ CacheSimulatorDMS.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+int logicalLog2(int x) //
+{
+int count = 0;
+
+while(x > 1)
+{
+x = x/2;
+count++;
+}
+return count;
+}
+
+
 int main(int argc, char *argv[])
 {
 FILE *traceFile;
@@ -12,5 +25,7 @@ traceFile = fopen("traceFile.txt", "r");
 fscanf(traceFile,"%[^\n]", firstLine);
 printf(firstLine);
 fclose(traceFile);
+
 }
+
 
