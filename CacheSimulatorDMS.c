fe2cc1e63d96a972b2c963b93d18be2422b0b617
Dylan Banh <dylanb271@gmail.com>
2018-09-11 00:53:46 +0100

    formatting

--- CacheSimulatorDMS.c
+++ CacheSimulatorDMS.c
@@ -6,27 +6,25 @@
 //Performs a logical logBASE2 on int argument and returns output
 int logicalLog2(int x) 
 {
-int count = 0;
+  int count = 0;
 
-while(x > 1)
-{
-x = x/2;
-count++;
-}
-return count;
+  while(x > 1)
+  {
+    x = x/2;
+    count++;
+  }
+  return count;
 }
 
 //Main
 int main(int argc, char *argv[])
 {
-FILE *traceFile;
-char firstLine[8];
+  FILE *traceFile;
+  char firstLine[8];
 
-traceFile = fopen("traceFile.txt", "r");
-fscanf(traceFile,"%[^\n]", firstLine);
-printf(firstLine);
-fclose(traceFile);
+  traceFile = fopen("traceFile.txt", "r");
+  fscanf(traceFile,"%[^\n]", firstLine);
+  printf(firstLine);
+  fclose(traceFile);
 
 }
-
-
